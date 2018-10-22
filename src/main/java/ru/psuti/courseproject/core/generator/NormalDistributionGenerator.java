64e5e914233e4c6adafff6401a90bd26d704ec32
Spmart <spmart@yandex.ru>
2018-10-22 23:29:02 +0400

    Создан класс NormalDistributionGenerator

---
+++ src/main/java/ru/psuti/courseproject/core/generator/NormalDistributionGenerator.java
@@ -0,0 +1,22 @@
+package ru.psuti.courseproject.core.generator;
+
+import ru.psuti.courseproject.core.pojo.CalculatedDataObject;
+
+import java.util.*;
+
+public class NormalDistributionGenerator implements Generator {
+
+    public NormalDistributionGenerator() {
+
+    }
+
+    @Override
+    public List<Double> getGeneratedRandomValues(int sampleSize) {
+        return null;
+    }
+
+    @Override
+    public List<CalculatedDataObject> getCalculatedData() {
+        return null;
+    }
+}
